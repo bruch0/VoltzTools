20efabb9274499b2bd09e634dc1aef0de6b6a7b6
bruch0 <lucas.bruch0@gmail.com>
2022-02-15 19:44:25 -0300

    fix: hashPassword was breaking joi validation

--- tests/factories/userFactory.js
+++ tests/factories/userFactory.js
@@ -4,14 +4,15 @@ import { hashSync } from 'bcrypt';
 import connection from '../../src/database/database';
 
 const createUser = async () => {
-  const hashPassword = hashSync(faker.internet.password(), 10);
+  const password = faker.internet.password();
+  const hashPassword = hashSync(password, 10);
 
   const user = await connection.query(
     'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *',
     [faker.name.findName(), faker.internet.email(), hashPassword]
   );
 
-  const { name, email, password } = user.rows[0];
+  const { name, email } = user.rows[0];
 
   return { name, email, password };
 };
