d2b05cdf70c889330c0bf4ed02c53a48fb65b50c
bruch0 <lucas.bruch0@gmail.com>
2022-02-15 16:15:39 -0300

    feat: add router and error handler to the app

--- src/app.js
+++ src/app.js
@@ -2,11 +2,17 @@ import express from 'express';
 import cors from 'cors';
 
 import toolsRoute from './routes/toolsRoute.js';
+import databaseError from './middlewares/databaseError.js';
+import inexistentRoute from './routes/inexistentRoute.js';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use(toolsRoute);
+
+app.use(inexistentRoute);
+
+app.use(databaseError);
 
 export default app;
