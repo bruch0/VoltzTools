e6e2edded67d0d5d169023095ee12bb23553ce82
bruch0 <lucas.bruch0@gmail.com>
2022-02-15 18:31:31 -0300

    feat: add new user route

---
+++ src/routes/userRoute.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import 'express-group-routes';
+import * as userController from '../controllers/userController.js';
+
+const router = new Router();
+
+router.group('/user', (route) => {
+  route.post('/register', userController.newUser);
+});
+
+export default router;
