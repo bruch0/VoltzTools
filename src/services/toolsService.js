413fd6bafb6b0506c66e1205aaa4ab2d036176a6
bruch0 <lucas.bruch0@gmail.com>
2022-02-15 17:18:32 -0300

    feat: add function to return the tool by id or throw error

--- src/services/toolsService.js
+++ src/services/toolsService.js
@@ -1,9 +1,19 @@
 import * as toolRepository from '../repositories/toolsRepository.js';
 
+import InexistentTool from '../errors/inexistentTool.js';
+
 const getAllTools = async () => {
   const tools = await toolRepository.getAllTools();
 
   return tools;
 };
 
-export { getAllTools };
+const getToolById = async ({ id }) => {
+  const tool = await toolRepository.getToolById({ id });
+
+  if (!tool.length) throw new InexistentTool();
+
+  return tool;
+};
+
+export { getAllTools, getToolById };
