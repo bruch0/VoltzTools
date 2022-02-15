40804b3ff44800087b7a09098fba2bdb623b83b0
bruch0 <lucas.bruch0@gmail.com>
2022-02-15 17:18:57 -0300

    feat: add function to return tool by id, wrap function on try catch block

--- src/controllers/toolsController.js
+++ src/controllers/toolsController.js
@@ -1,9 +1,28 @@
 import * as toolsService from '../services/toolsService.js';
 
-const getAllTools = async (req, res) => {
-  const tools = await toolsService.getAllTools();
+const getAllTools = async (req, res, next) => {
+  try {
+    const tools = await toolsService.getAllTools();
 
-  return res.send(tools);
+    return res.send(tools);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export { getAllTools };
+const getToolById = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const tool = await toolsService.getToolById({ id });
+
+    return res.send(tool);
+  } catch (error) {
+    if ((error.name = 'inexistentTool'))
+      return res.status(404).send(error.message);
+
+    next(error);
+  }
+};
+
+export { getAllTools, getToolById };
