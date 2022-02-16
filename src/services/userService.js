b64662349aa36c224482c5c449a540d2d9be7c0e
bruch0 <lucas.bruch0@gmail.com>
2022-02-16 10:44:15 -0300

    feat: add logging in function on service level

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,11 +1,15 @@
 import translate from '@vitalets/google-translate-api';
 import BodyValidation from '../errors/bodyValidation.js';
-import { hashSync } from 'bcrypt';
+import { compareSync, hashSync } from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 import * as userRepository from '../repositories/userRepository.js';
+import * as sessionRepository from '../repositories/sessionRepository.js';
 
 import * as userSchema from '../validations/userSchema.js';
 import EmailAlreadyTaken from '../errors/emailAlreadyTaken.js';
+import InexistentUser from '../errors/inexistentUser.js';
+import IncorrectPassword from '../errors/incorrectPassword.js';
 
 const newUser = async ({ name, email, password }) => {
   const validation = userSchema.createUser.validate({ name, email, password });
@@ -28,4 +32,32 @@ const newUser = async ({ name, email, password }) => {
   return;
 };
 
-export { newUser };
+const login = async ({ email, password }) => {
+  const validation = userSchema.login.validate({ email, password });
+
+  if (validation.error) {
+    const errorMessage = validation.error.details[0].message;
+
+    await translate(errorMessage, { from: 'en', to: 'pt' }).then((res) => {
+      throw new BodyValidation(res.text);
+    });
+  }
+
+  const user = await userRepository.findUser({ email });
+  if (!user) throw new InexistentUser();
+
+  const dbPassword = user.password;
+  const passwordMatch = compareSync(password, dbPassword);
+  if (!passwordMatch) throw new IncorrectPassword();
+
+  const userId = user.id;
+  await sessionRepository.registerUserSession({ userId });
+
+  const jwtSecret = process.env.JWT_SECRET;
+  const configurations = { expiresIn: 60 * 60 * 24 };
+  const token = jwt.sign({}, jwtSecret, configurations);
+
+  return token;
+};
+
+export { newUser, login };
