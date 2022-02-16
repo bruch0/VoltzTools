2c88e36dcc8287ed4477ec85b47e0e199d0b3bee
bruch0 <lucas.bruch0@gmail.com>
2022-02-16 14:56:27 -0300

    feat: add required for the object properties

--- src/validations/userSchema.js
+++ src/validations/userSchema.js
@@ -1,14 +1,14 @@
 import joi from 'joi';
 
 const createUser = joi.object({
-  name: joi.string().required().min(3).max(30),
-  email: joi.string().required().email(),
-  password: joi.string().required().min(8).max(50),
+  name: joi.string().required().min(3).max(30).required(),
+  email: joi.string().required().email().required(),
+  password: joi.string().required().min(8).max(50).required(),
 });
 
 const login = joi.object({
-  email: joi.string().required().email(),
-  password: joi.string().required().min(8).max(50),
+  email: joi.string().required().email().required(),
+  password: joi.string().required().min(8).max(50).required(),
 });
 
 export { createUser, login };
