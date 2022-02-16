b636128f4c2f12f006ff2a73e82f33cfad9a3a3a
bruch0 <lucas.bruch0@gmail.com>
2022-02-16 15:14:46 -0300

    feat: add jwt middleware and create tool on toolsRoute

--- src/routes/toolsRoute.js
+++ src/routes/toolsRoute.js
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 import 'express-group-routes';
+
 import * as toolsController from '../controllers/toolsController.js';
+
+import authJwt from '../middlewares/authentication.js';
 
 const router = new Router();
 
 router.group('/tools', (route) => {
   route.get('', toolsController.getAllTools);
   route.get('/:id', toolsController.getToolById);
+
+  route.use(authJwt);
+  route.post('', toolsController.createTool);
 });
 
 export default router;
