ae7d0247a7023477e44a6584c4d106c62fe68b95
bruch0 <lucas.bruch0@gmail.com>
2022-02-16 10:55:27 -0300

    tests: add logging in integration tests

--- tests/integration/userController.test.js
+++ tests/integration/userController.test.js
@@ -46,3 +46,89 @@ describe('POST /user/register', () => {
     expect(result.status).toEqual(409);
   });
 });
+
+describe('POST /user/login', () => {
+  it('should return status 400 when sending invalid body', async () => {
+    const result = await request.post('/user/login');
+
+    expect(result.status).toEqual(400);
+  });
+
+  it('should return status 404 when the email is not registered', async () => {
+    await clearUsers();
+
+    const body = {
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    const result = await request.post('/user/login').send(body);
+
+    expect(result.status).toEqual(404);
+  });
+
+  it('should return status 401 when password does not match', async () => {
+    const { email } = await createUser();
+
+    const body = {
+      email,
+      password: faker.internet.password(),
+    };
+
+    const result = await request.post('/user/login').send(body);
+
+    expect(result.status).toEqual(401);
+  });
+
+  it('should return status 200 when password match', async () => {
+    const { email, password } = await createUser();
+
+    const body = {
+      email,
+      password,
+    };
+
+    const result = await request.post('/user/login').send(body);
+
+    expect(result.status).toEqual(200);
+  });
+
+  it('should return an object when successfully logging in', async () => {
+    const { email, password } = await createUser();
+
+    const body = {
+      email,
+      password,
+    };
+
+    const result = await request.post('/user/login').send(body);
+
+    expect(typeof result.body).toBe('object');
+  });
+
+  it('should return an token when successfully logging in', async () => {
+    const { email, password } = await createUser();
+
+    const body = {
+      email,
+      password,
+    };
+
+    const result = await request.post('/user/login').send(body);
+
+    expect(result.body).toHaveProperty('token');
+  });
+
+  it('should return the token as an string', async () => {
+    const { email, password } = await createUser();
+
+    const body = {
+      email,
+      password,
+    };
+
+    const result = await request.post('/user/login').send(body);
+
+    expect(typeof result.body.token).toBe('string');
+  });
+});
