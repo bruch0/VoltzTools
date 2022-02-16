bf927706643ddcb09b93d03db030358b7ca6346c
bruch0 <lucas.bruch0@gmail.com>
2022-02-16 15:14:18 -0300

    feat: add function on service level to create tool

--- src/services/toolsService.js
+++ src/services/toolsService.js
@@ -1,6 +1,9 @@
 import * as toolRepository from '../repositories/toolsRepository.js';
 
+import * as toolSchema from '../validations/toolSchema.js';
+
 import InexistentTool from '../errors/inexistentTool.js';
+import BodyValidation from '../errors/bodyValidation.js';
 
 const getAllTools = async () => {
   const tools = await toolRepository.getAllTools();
@@ -16,4 +19,25 @@ const getToolById = async ({ id }) => {
   return tool;
 };
 
-export { getAllTools, getToolById };
+const createTool = async ({ title, link, description, tags }) => {
+  const validation = toolSchema.createTool.validate({
+    title,
+    link,
+    description,
+    tags,
+  });
+
+  if (validation.error) {
+    const errorMessage = validation.error.details[0].message;
+
+    await translate(errorMessage, { from: 'en', to: 'pt' }).then((res) => {
+      throw new BodyValidation(res.text);
+    });
+  }
+
+  await toolRepository.createTool({ title, link, description, tags });
+
+  return;
+};
+
+export { getAllTools, getToolById, createTool };
