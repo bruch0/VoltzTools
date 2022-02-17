ab94ebeebaf6ef70fac5396a92123fc730efa908
bruch0 <lucas.bruch0@gmail.com>
2022-02-17 08:19:35 -0300

    tests: add unit tests for getting logs by userId

--- tests/unit/logsService.test.js
+++ tests/unit/logsService.test.js
@@ -1,3 +1,5 @@
+import faker from 'faker';
+
 import * as logsRepository from '../../src/repositories/logsRepository.js';
 import * as logsService from '../../src/services/logsService.js';
 
@@ -5,6 +7,20 @@ describe('logsService - get the logs', () => {
   it('should return an array', async () => {
     jest.spyOn(logsRepository, 'getLogs').mockImplementationOnce(() => []);
     const result = await logsService.getLogs();
+
+    await expect(Array.isArray(result)).toBe(true);
+  });
+});
+
+describe('logsService - get logs by userId', () => {
+  it('should return an array with the logs', async () => {
+    const randomId = faker.datatype.number();
+
+    jest
+      .spyOn(logsRepository, 'getLogsByUserId')
+      .mockImplementationOnce(() => []);
+
+    const result = await logsRepository.getLogsByUserId({ randomId });
 
     await expect(Array.isArray(result)).toBe(true);
   });
