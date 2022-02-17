425bc2a2b2ccbc65d7bb9615099d3e4da30c1889
bruch0 <lucas.bruch0@gmail.com>
2022-02-16 22:04:51 -0300

    feat: add logsRoute

---
+++ src/routes/logsRoute.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import 'express-group-routes';
+
+import * as logsController from '../controllers/logsController.js';
+
+import authJWT from '../middlewares/authentication.js';
+
+const router = new Router();
+
+router.group('/logs', (route) => {
+  route.use(authJWT);
+  route.get('', logsController.getLogs);
+});
+
+export default router;
