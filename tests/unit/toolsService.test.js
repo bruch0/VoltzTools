794043d316c85a8aa788e02c8865454fffcd1af4
bruch0 <lucas.bruch0@gmail.com>
2022-02-17 08:42:23 -0300

    fix: add mock to new function on repository

--- tests/unit/toolsService.test.js
+++ tests/unit/toolsService.test.js
@@ -9,8 +9,12 @@ import BodyValidation from '../../src/errors/bodyValidation.js';
 
 describe('toolService - get tool by id', () => {
   it('should throw an error when the id is not found', async () => {
+    jest
+      .spyOn(toolsRepository, 'getToolById')
+      .mockImplementationOnce(() => false);
+    jest.spyOn(toolsRepository, 'getTagsById').mockImplementationOnce(() => []);
+
     const id = faker.datatype.number();
-    jest.spyOn(toolsRepository, 'getToolById').mockImplementationOnce(() => []);
 
     const promise = toolsService.getToolById({ id });
 
@@ -18,10 +22,12 @@ describe('toolService - get tool by id', () => {
   });
 
   it('should return an array the id is found', async () => {
-    const id = faker.datatype.number();
     jest
       .spyOn(toolsRepository, 'getToolById')
       .mockImplementationOnce(() => [faker.datatype.number()]);
+    jest.spyOn(toolsRepository, 'getTagsById').mockImplementationOnce(() => []);
+
+    const id = faker.datatype.number();
 
     const result = await toolsService.getToolById({ id });
 
